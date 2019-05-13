d9b143a28d4f8e423e2facba27d227d7f23f1864
Tim Makin <timmakin@Tims-New-MacBook-Pro.local>
2019-05-13 15:39:04 +0100

    can send and display images

--- scenes/messaging/Chat.scene.js
+++ scenes/messaging/Chat.scene.js
@@ -6,7 +6,7 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
+    ImageBackground,
   View
 } from 'react-native'
 
@@ -14,6 +14,8 @@ import { Container, Header, Content, Form, Item, Input, Label, Left, Body, Right
 //import getTheme from './native-base-theme/components';
 //import material from './native-base-theme/variables/material';
 import { Col, Row, Grid } from 'react-native-easy-grid'
+
+import ImagePicker from 'react-native-image-picker'
 
 import _ from 'lodash'
 import moment from 'moment'
@@ -43,7 +45,8 @@ const mapDispatchToProps = (dispatch) => ({
 class ConversationsScene extends Component {
   
     state = {
-        messages: []
+        messages: [],
+        selectedImage: {}
     }
 
     componentWillMount() {
@@ -71,7 +74,7 @@ class ConversationsScene extends Component {
         _.each(messageData, (msg, index) => {
             var formattedMsg = {
                 _id: index,
-                text: msg.Message,
+                text: msg.MessageType === "I" ? "" : msg.Message,
                // image: 'https://placeimg.com/540/340/any',
                 createdAt: new Date(msg.MessageDate),
                 user: {
@@ -80,6 +83,10 @@ class ConversationsScene extends Component {
                   avatar: "https://placeimg.com/140/140/any"
                 }
               }
+
+            if (msg.MessageType === "I") {
+                formattedMsg.image =  msg.Message
+            }
             formattedMsgs.push(formattedMsg)
         })
 
@@ -91,6 +98,67 @@ class ConversationsScene extends Component {
             nextProps.singleMessage.type === actionTypes.CREATED_RESPONE_SUCCESS) {
             this.props.selectMessage(this.props.singleMessage.meta)
         }
+    }
+
+    pickImage = () => {
+
+        var options = {
+            title: 'Select image',
+            quality: 0.5,
+            customButtons: [],
+            storageOptions: {
+              skipBackup: true,
+              path: 'images'
+            }
+          };
+
+          ImagePicker.showImagePicker(options, (response) => {
+    
+            console.log(response)
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+      
+              let source = {
+                data: response.data,
+                height: response.height,
+                width: response.width
+              }
+
+              this.setState({
+                selectedImage: response
+              })
+          
+              // You can also display the image using data:
+              // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+      
+            }
+          });
+          
+
+    }
+
+    renderActions = () => {
+        return (<Button transparent onPress={this.pickImage}>
+            <Icon name='camera' />
+          </Button>)
+    }
+
+    cancelImage = () => {
+        this.setState({
+            selectedImage: {}
+        })
+    }
+
+    sendImage = () => {
+        this.props.createResponse(this.props.singleMessage.meta.ChaId, 'I', this.state.selectedImage.data, this.props.authReducer.userId)
+        this.setState({
+            selectedImage: {}
+        })
     }
 
   render () {
@@ -109,16 +177,30 @@ class ConversationsScene extends Component {
            
           </Right>
         </Header>
-            <View style={{ flex: 1 }}>
+            {this.state.selectedImage.uri && <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, backgroundColor:'yellow' }}>
+                    <ImageBackground source={{ uri: this.state.selectedImage.uri }} style={{ width: '100%', height: '100%' }} />
+                </View>
+                <View style={{ paddingBottom: 20, flexDirection: 'row' }}>
+                    <Button onPress={this.cancelImage} style={{ ...Styles.SECONDARY_BUTTON, borderRadius: 0, flex: 1, justifyContent: 'center' }}>
+                        <Text style={Styles.SECONDARY_BUTTON_TEXT}>Cancel</Text>
+                    </Button>
+                    <Button onPress={this.sendImage} style={{ ...Styles.PRIMARY_BUTTON, borderRadius: 0, flex: 1, justifyContent: 'center' }}>
+                        <Text style={{ ...Styles.PRIMARY_BUTTON_TEXT, textAlign: 'center' }}>Send</Text>
+                    </Button>
+                </View>
+            </View>}
+            {!this.state.selectedImage.uri && <View style={{ flex: 1 }}>
                 <GiftedChat
                 renderUsernameOnMessage
                 messages={this.state.messages}
+                renderActions={this.renderActions}
                 onSend={messages => this.onSend(messages)}
                 user={{
                 _id: this.props.authReducer.userId
                 }}
             />
-            </View>
+            </View>}
         </Container>
     );
   }
